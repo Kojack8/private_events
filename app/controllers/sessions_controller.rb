befd39d3fcb96011564b87cf61f13ddcb21a0db5
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-04 18:19:55 -0800

    Added simple log-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+  def create 
+    @user = User.find_by(name: params[:name])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect_to :controller => 'users', :action => 'show', :id => session[:user_id]
+    else
+      redirect_to '/login'
+    end
+  end
 end
