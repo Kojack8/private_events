c468d1332ecb1003c0bee632903f0f2b6d3ff86c
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-08 22:46:00 -0600

    updated event index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,14 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+  
   def new
     @event = Event.new
   end
 
   def show
     @event = Event.find(params[:id])
-  end
-
-  def index
-    @events = Event.all
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+
   root to: "users#new"
   resources :users, only: [:new, :create, :show]
-  resources :events, only: [:show, :new, :create, :index]
+  resources :events, only: [:new, :create, :index, :show]
   get 'login', to: 'sessions#new'   
   post 'login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
