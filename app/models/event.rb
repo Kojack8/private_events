2031aa00d167d88a2c82bcb24367211ce2e85833
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-14 00:50:56 -0600

    Added :upcoming & :past scope to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: 'User', foreign_key: :user_id
   has_many :participation
   has_many :attendees, through: :participation, source: :user
+
+  scope :upcoming, -> { where('time > ?', Time.now).order(time: :desc)}
+  scope :past, -> { where('time < ?', Time.now).order(:time)}
 end
