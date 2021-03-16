66bdf89a3a862f739103af241e6e12105b46afc8
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-16 13:52:28 -0500

    removed has_many: invitees

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: 'User', foreign_key: :user_id
   has_many :participation
   has_many :attendees, through: :participation, source: :user
-  has_many :invitees, class_name: 'User', foreign_key: :id
+
 
   scope :upcoming, -> { where('time > ?', Time.now).order(time: :desc)}
   scope :past, -> { where('time < ?', Time.now).order(:time)}
