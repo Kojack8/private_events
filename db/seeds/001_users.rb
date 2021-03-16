ff479564aeb056345f197e40002c0359fd28ae4d
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-16 14:43:42 -0500

    Added seeds

---
+++ db/seeds/001_users.rb
@@ -0,0 +1,18 @@
+User.find_or_create_by(name: "Tina")
+
+User.find_or_create_by(name: "Teddy")
+
+User.find_or_create_by(name: "Tammy")
+
+User.find_or_create_by(name: "Glencrest Yacht Club")
+
+User.find_or_create_by(name: "Louise")
+
+User.find_or_create_by(name: "Gene")
+
+User.find_or_create_by(name: "Hugo")
+
+User.find_or_create_by(name: "Linda")
+
+User.find_or_create_by(name: "Bob")
+
---
+++ db/seeds/002_events.rb
@@ -0,0 +1,53 @@
+Event.find_or_create_by(title: "Dad\'s B-day Party") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 1
+end
+
+Event.find_or_create_by(title: "Teddy's Halloween Party") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+Event.find_or_create_by(title: "Tammy's Bat Mitzvah") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+Event.find_or_create_by(title: "Louise's Science Fair/ Gene's 'Topsy' ") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+Event.find_or_create_by(title: "Yacht Club Party ") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+Event.find_or_create_by(title: "Tammy's Anti-Valentine's Day Party ") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+Event.find_or_create_by(title: "Lobsterfest ") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+Event.find_or_create_by(title: "Mother-Daughter Seminar ") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+Event.find_or_create_by(title: "Boy's For Now Concert ") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+Event.find_or_create_by(title: "Tina's Yellow Cord Ceremony ") do |e|
+  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.user_id = 2
+end
+
+
+
+
