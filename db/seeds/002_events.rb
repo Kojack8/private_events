0db6d78aa1a0e158e7703cd0f7f9641ee16b3801
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-16 15:39:45 -0500

    updated time value on seeds

--- db/seeds/002_events.rb
+++ db/seeds/002_events.rb
@@ -1,55 +1,55 @@
-Event.find_or_create_by(title: "Dad\'s B-day Party") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+Event.find_or_create_by(title: "Dad\'s birthday day Party") do |e|
+  e.time = "2021-05-01 18:30:00.000000000 +0000"
   e.user_id = 1
 end
 
 Event.find_or_create_by(title: "Teddy's Halloween Party") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2021-10-31 19:00:00.000000000 +0000"
   e.user_id = 2
 end
 
 Event.find_or_create_by(title: "Tammy's Bat Mitzvah") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2021-08-15 12:00:00.000000000 +0000"
   e.user_id = 3
 end
 
 Event.find_or_create_by(title: "Louise's Science Fair/ Gene's 'Topsy' ") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2019-10-03 16:00:00.000000000 +0000"
   e.user_id = 4
 end
 
 Event.find_or_create_by(title: "Commodore's Ball ") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2022-08-05 19:30:00.000000000 +0000"
   e.user_id = 5
 end
 
 Event.find_or_create_by(title: "Tammy's Anti-Valentine's Day Party ") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2021-02-14 18:00:00.000000000 +0000"
   e.user_id = 3
 end
 
 Event.find_or_create_by(title: "Lobsterfest ") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2021-07-02 10:00:00.000000000 +0000"
   e.user_id = 7
 end
 
 Event.find_or_create_by(title: "Bobsterfest ") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2021-07-02 11:00:00.000000000 +0000"
   e.user_id = 9
 end
 
 Event.find_or_create_by(title: "Mother-Daughter Seminar ") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2022-03-16 15:30:00.000000000 +0000"
   e.user_id = 8
 end
 
 Event.find_or_create_by(title: "Boy's For Now Concert ") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2021-12-01 19:00:00.000000000 +0000"
   e.user_id = 1
 end
 
 Event.find_or_create_by(title: "Tina's Yellow Cord Ceremony ") do |e|
-  e.time = "2021-03-16 19:04:00.000000000 +0000"
+  e.time = "2022-01-16 12:00:00.000000000 +0000"
   e.user_id = 1
 end
 
